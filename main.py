a02f353d934230606093d8976847eec26ea3ebef
Noriks29 <sehinr292@gmail.com>
2025-09-19 18:07:17 +0300

    без поддомена

--- main.py
+++ main.py
@@ -10,20 +10,18 @@ os.mkdir("map")
 # https://a.tile-cyclosm.openstreetmap.fr/cyclosm/1/1/1.png
 zmax = 5
 zmin = 1
-urlbase = '.tile-cyclosm.openstreetmap.fr/cyclosm/'
+urlbase = 'a.tile-cyclosm.openstreetmap.fr/cyclosm/'
 
 for z in range(zmin,zmax+1):
     os.makedirs("map/"+str(z))
-    for s in ['a','b','c']:
-        os.makedirs("map/"+str(z)+'/'+str(s))
-        for x in range(0, 2 ** z):
-            os.makedirs("map/"+str(z)+'/'+str(s)+'/'+str(x))
-            for y in range(0, 2 ** z):
-                url = 'https://'+str(s)+urlbase+str(z)+'/'+str(x)+'/'+str(y)+'.png'
-                data = requests.get(url).content
-                print(url)
-                f = open("map/"+str(z)+'/'+str(s)+'/'+str(x)+'/'+str(y)+'.png','wb')
-                # Storing the image data inside the data variable to the file
-                f.write(data)
-                f.close()
-                #open("map/1/text.txt", "w")
+    for x in range(0, 2 ** z):
+        os.makedirs("map/"+str(z)+'/'+str(x))
+        for y in range(0, 2 ** z):
+            url = 'https://'+urlbase+str(z)+'/'+str(x)+'/'+str(y)+'.png'
+            data = requests.get(url).content
+            print(url)
+            f = open("map/"+str(z)+'/'+str(x)+'/'+str(y)+'.png','wb')
+            # Storing the image data inside the data variable to the file
+            f.write(data)
+            f.close()
+            #open("map/1/text.txt", "w")
